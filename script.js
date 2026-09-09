80083de4dc33a3cfa3fd93101af53ccee8f51783
vaejasmi <jasjasmi253@gmail.com>
2026-09-08 18:29:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const signalButton = document.querySelector("#signal-button");
+const domeStatus = document.querySelector("#dome-status");
+
+<button onclick="alert('Hello!')">Click Me</button>
+
+
+// TODO: Add a click event listener.
+// When the learner presses the button, change domeStatus.textContent so the
+// next crew can confirm the dome is online or read its next instruction.
